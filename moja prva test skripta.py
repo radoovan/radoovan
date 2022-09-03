8aa94b2196c377c657eec5e36dd14211d085b9a3
Radovan Zindovic <112682921+radoovan@users.noreply.github.com>
2022-09-03 15:11:35 +0200

    Add files via upload

---
+++ moja prva test skripta.py
@@ -0,0 +1,24 @@
+
+from pyautogui import unittest
+
+class testString:
+    
+    methods = (unittest.testcase. pyautogui)
+
+    def testlower(self):
+        self.assertEqual('FOO'.lower(),'foo')
+   
+    def testlower(self):
+        self.assertTrue('foo'.lower())
+        self.assertFalse('FOO'.lower())
+
+
+    def test_split(self):
+        s = 'good mornning'
+        self.assertEqual(s.split(),['good','morning'])
+        self.split (2)
+
+if __name__ == '__main__':
+        unittest.main ()
+        
+    
